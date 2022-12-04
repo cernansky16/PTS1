e37c0bf2b14a0b1244f010e77e9285e451ebefb9
cernansky16 <andrejcernansky7@gmail.com>
2022-12-04 12:10:00 +0100

    I have started implementing classes which are used by players and observers.

---
+++ GameObservable.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING,List
+
+if TYPE_CHECKING:
+    from Position import HandPosition, SleepingQueenPosition,AwokenQueenPosition
+from Game import Game
+
+class GameObserverInterface:
+    def notify(self, message: str):
+        pass
+
+class GameObservable(GameObserverInterface):
+    def __init__(self):
+        self.players = list()
+        self.observers = list()
+    def add(self,observer) -> None:
+        self.observers.append(observer)
+    def addPlayer(self,player,observer) -> None:
+        self.observers.append(observer)
+        self.players.append(player)
+    def remove(self,observer) -> None:
+        self.observers.remove(observer)
+    def notifyAll(self,message) -> None:
+        pass
+class GamePlayerInterface:
+    def play(self,player : str, cards: str):
+        pass
+
+class GameAdaptor(GamePlayerInterface):
+    def __init__(self):
+        self.game = Game()
+    def play(self,player : str, cards: str) ->None:
+        index = int(player)
+        player = self.game.players[index-1]
+        commands = cards.split()
+        def getPositions():
+            handpos : List[HandPosition] = []
+            sleepingpos = None
+            awokenqueenpos = None
+            for command in commands:
+                if command[0] == "h":
+                    index = int(command[1])
+                    pos = HandPosition(index,player)
+                    handpos.append(pos)
+                elif command[0] == "a":
+                    index = int(command[1])
+                    playerIdx = int(command[2])
+                    awokenqueenpos = AwokenQueenPosition(index,self.game.players[playerIdx-1])
+                elif command[0] == "s":
+                    index = int(command[1])
+                    sleepingpos = SleepingQueenPosition(index)
+                return [handpos, sleepingpos, awokenqueenpos]
+
+        self.game.play(index,getPositions())
+
+class GameFinishedInterface:
+    def isFinished(self) -> bool:
+       pass
+class GameFinished(GameFinishedInterface):
+    pass
+
+
+
+
